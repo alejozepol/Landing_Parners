f1c40330353ed4b0b121f7f5e195e2d60806885f
Alejandro Lopez <alejozepol@gmail.com>
2019-11-13 02:45:14 -0500

    creacion de modal problema de cierre

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Contenido from '../components/Contenido';
+import Modal from '../components/modal';
 import Logo from '../assets/static/logo.png';
 import logoWs from '../assets/static/logoWS.png';
 import '../assets/styles/index.scss';
@@ -8,6 +9,7 @@ const Pages = () => {
   const contenido = {
     title: ['Descubre', 'Conéctate', 'Practica'],
     frase: ['espacios, eventos y actividades', 'con personas, equipos y grupos', 'tu deporte favorito'],
+    modal: false,
   };
 
   const [text, setText] = useState({
@@ -15,30 +17,81 @@ const Pages = () => {
     frase: [contenido.frase[0]],
 
   });
-  if (text.title == contenido.title[0]) {
+  const viewModal = () => {
+    setText({
+      ...text,
+      modal: true,
+    });
+  };
+/* 
+  if (text.title == contenido.title[0] && !text.modal) {
     setTimeout(() => {
       setText({
+        ...text,
         title: [contenido.title[1]],
         frase: [contenido.frase[1]],
       });
-    }, 3000);
+    }, 5000);
+  };
+
+  if (text.title == contenido.title[1] && !text.modal) {
     setTimeout(() => {
       setText({
+        ...text,
         title: [contenido.title[2]],
         frase: [contenido.frase[2]],
       });
-    }, 6000);
+    }, 5000);
+  };
+
+  if (text.title == contenido.title[2] && !text.modal) {
     setTimeout(() => {
       setText({
+        ...text,
         title: [contenido.title[0]],
         frase: [contenido.frase[0]],
       });
-    }, 8000);
-  }
+    }, 5000);
+  } */
 
+  do {
+    if (text.title == contenido.title[0] && !text.modal) {
+      setTimeout(() => {
+        setText({
+          ...text,
+          title: [contenido.title[1]],
+          frase: [contenido.frase[1]],
+        });
+      }, 5000);
+    };
+    if (text.title == contenido.title[1] && !text.modal) {
+      setTimeout(() => {
+        setText({
+          ...text,
+          title: [contenido.title[2]],
+          frase: [contenido.frase[2]],
+        });
+      }, 5000);
+    };
+
+    if (text.title == contenido.title[2] && !text.modal) {
+      setTimeout(() => {
+        setText({
+          ...text,
+          title: [contenido.title[0]],
+          frase: [contenido.frase[0]],
+        });
+      }, 5000);
+    }
+  } while (!text.modal);
   return (
     <Contenido>
       <section className='Pages'>
+        {text.modal && (
+          <Modal>
+            <h3>Modalidad de Pago</h3>
+          </Modal>
+        )}
         <img className='Pages__img' src={Logo} alt='Logo Parners' />
         <div>
           <h2 className='Pages__title words'>{text.title}</h2>
@@ -46,10 +99,16 @@ const Pages = () => {
         </div>
         <h4 className='Pages__cta'>¡Entrenemos Juntos!</h4>
         <div className='Pages__btn'>
-          <button className='btn btn-blanco-color' type='button'>Descubre como </button>
+          <button
+            className='btn btn-blanco-color'
+            type='button'
+            onClick={() => viewModal(text.modal)}
+          >
+            Descubre como
+          </button>
           <button className='btn btn-whatsapp' type='button'>
             <img src={logoWs} alt='LogoWhatsapp' />
-            Contactanos 
+            Contactanos
           </button>
         </div>
       </section>
