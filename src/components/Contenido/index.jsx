ccfd3321ebbd76b2ae671332ac276b238bfb9bc6
Alejandro Lopez <alejozepol@gmail.com>
2019-11-13 10:26:39 -0500

    arreglo diseño movil

--- src/components/Contenido/index.jsx
+++ src/components/Contenido/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Contenido.scss';
 import gym from '../../assets/static/gym.jpg';
+import gymMovil from '../../assets/static/fondoMovil.png';
 
 const Contenido = ({ children }) => {
   return (
     <section className='Contenido'>
-      <img src={gym} alt={gym} className='Contenido__img' />
+      <img src={gym} alt='fondo Deporte' className='Contenido__img' />
+      <img src={gymMovil} alt='fondo Deporte' className='Contenido__img-movil' />
       {children}
     </section>
   );
