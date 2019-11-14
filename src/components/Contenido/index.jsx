fa7a16dfc88e5a2b08ff9a1495598903d3a511ce
Alejandro Lopez <alejozepol@gmail.com>
2019-11-14 13:00:54 -0500

    implementacion de bg para movil

--- src/components/Contenido/index.jsx
+++ src/components/Contenido/index.jsx
@@ -1,40 +1,26 @@
 import React, { useState } from 'react';
 import './Contenido.scss';
-import bg1 from '../../assets/static/1.jpg';
-import bg2 from '../../assets/static/2.jpg';
-import bg3 from '../../assets/static/3.jpg';
-import bg4 from '../../assets/static/4.jpg';
-import bg5 from '../../assets/static/5.jpg';
-import bg6 from '../../assets/static/6.jpg';
-import bg7 from '../../assets/static/7.jpg';
-import bg8 from '../../assets/static/8.jpg';
-import gymMovil from '../../assets/static/fondoMovil.png';
+import bg1 from '../../assets/static/1.png';
+import bg2 from '../../assets/static/2.png';
+import bg3 from '../../assets/static/3.png';
+import bgm1 from '../../assets/static/m1.png';
+import bgm2 from '../../assets/static/m2.png';
+import bgm3 from '../../assets/static/m3.png';
 
 const Contenido = ({ children }) => {
   const [bg, setBg] = useState(bg1);
+  const [bgm, setBgm] = useState(bgm1);
 
   if (bg == bg1) {
     setTimeout(() => {
       setBg(bg2);
+      setBgm(bgm2);
       setTimeout(() => {
         setBg(bg3);
+        setBgm(bgm3);
         setTimeout(() => {
-          setBg(bg4);
-          setTimeout(() => {
-            setBg(bg5);
-            setTimeout(() => {
-              setBg(bg6);
-              setTimeout(() => {
-                setBg(bg7);
-                setTimeout(() => {
-                  setBg(bg8);
-                  setTimeout(() => {
-                    setBg(bg1);
-                  }, 3000);
-                }, 3000);
-              }, 3000);
-            }, 3000);
-          }, 3000);
+          setBg(bg1);
+          setBgm(bgm1);
         }, 3000);
       }, 3000);
     }, 3000);
@@ -42,7 +28,7 @@ const Contenido = ({ children }) => {
   return (
     <section className='Contenido'>
       <img src={bg} alt='fondo Deporte' className='Contenido__img' />
-      <img src={gymMovil} alt='fondo Deporte' className='Contenido__img-movil' />
+      <img src={bgm} alt='fondo Deporte' className='Contenido__img-movil' />
       {children}
     </section>
   );
